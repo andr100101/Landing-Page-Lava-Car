03f8cf31475c505aa42a67a7d54a556a9adb7d11
andr100101 <andreluizreis2906@gmail.com>
2026-04-12 19:25:16 -0300

    algumas correções e efeitos visuais :)

--- scripts/effects.js
+++ scripts/effects.js
@@ -1,6 +1,10 @@
 const maps = document.getElementById("maps-integration")
 const landOverlay = document.getElementById("overlay");
 const services = document.querySelectorAll(".service-block");
+const mensageBox = document.getElementById("mensage-box");
+const markTxt = document.getElementById("mark-txt");
+const comparisons = document.querySelectorAll(".comparison-block");
+
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -8,14 +12,31 @@ window.addEventListener('scroll', () => {
     if(window.scrollY > 30){
         maps.classList.add('scroll');
         landOverlay.classList.add('scroll');
+        
+        if(window.scrollY > 370){
         services.forEach(service => {
             service.classList.add('move');
-        })}else {
+        });
+        if(window.scrollY > 1600){
+            mensageBox.style.transform = "translateY(0%)";
+            if(window.scrollY > 1800){
+                markTxt.style.transform = "translateX(0%)";
+            }else{
+               markTxt.style.transform = "translateX(100%)" 
+            }
+
+        }else{
+            mensageBox.style.transform = "translateY(140%)";
+        }}else{
+            services.forEach(service =>{
+            service.classList.remove('move');
+        })
+        }
+    
+    }else {
         maps.classList.remove('scroll');
         landOverlay.classList.remove('scroll');
-        services.forEach(service =>{
-            service.classList.remove('move');
-        })};
+        };
     }
 );
 

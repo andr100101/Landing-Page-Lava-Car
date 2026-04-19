4714cd50238a693dfb63350beac4d0969d3c320f
andr100101 <andreluizreis2906@gmail.com>
2026-04-19 20:59:28 -0300

    Alguns ajustes de design e responsividade, remoção de valores fixos na largura e altura de objetos

--- scripts/effects.js
+++ scripts/effects.js
@@ -4,28 +4,31 @@ const services = document.querySelectorAll(".service-block");
 const mensageBox = document.getElementById("mensage-box");
 const markTxt = document.getElementById("mark-txt");
 const comparisons = document.querySelectorAll(".comparison-block");
+const review = document.getElementById("review");
 
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 window.addEventListener('scroll', () => {
-    if(window.scrollY > 30){
+    if(window.scrollY > 40){
         maps.classList.add('scroll');
         landOverlay.classList.add('scroll');
         
-        if(window.scrollY > 370){
+        if(window.scrollY > 430){
         services.forEach(service => {
             service.classList.add('move');
         });
-        if(window.scrollY > 1600){
+        if(window.scrollY > 2200){
+            review.style.transform = "translateX(0%)"
             mensageBox.style.transform = "translateY(0%)";
-            if(window.scrollY > 1800){
+            if(window.scrollY > 2400){
                 markTxt.style.transform = "translateX(0%)";
             }else{
                markTxt.style.transform = "translateX(100%)" 
             }
 
         }else{
+            review.style.transform = "translateX(-100%)";
             mensageBox.style.transform = "translateY(140%)";
         }}else{
             services.forEach(service =>{

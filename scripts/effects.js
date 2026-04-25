aa29367321a28e7e6cd79e58a912e32c01248941
andr100101 <andreluizreis2906@gmail.com>
2026-04-24 22:51:00 -0300

    Adição do instagram na tela iniciale ajustes pequenos no css

--- scripts/effects.js
+++ scripts/effects.js
@@ -10,18 +10,18 @@ const review = document.getElementById("review");
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 window.addEventListener('scroll', () => {
-    if(window.scrollY > 40){
+    if(window.scrollY > 800){
         maps.classList.add('scroll');
         landOverlay.classList.add('scroll');
         
-        if(window.scrollY > 430){
+        if(window.scrollY > 900){
         services.forEach(service => {
             service.classList.add('move');
         });
-        if(window.scrollY > 2200){
+        if(window.scrollY > 2500){
             review.style.transform = "translateX(0%)"
             mensageBox.style.transform = "translateY(0%)";
-            if(window.scrollY > 2400){
+            if(window.scrollY > 3000){
                 markTxt.style.transform = "translateX(0%)";
             }else{
                markTxt.style.transform = "translateX(100%)" 
